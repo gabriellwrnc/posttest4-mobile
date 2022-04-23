af2a11bafa1f9386c82c34d62066250ae0917a13
gabriellwrnc <gabriel.jamrewav@gmail.com>
2022-04-23 15:52:14 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/posttest4_1915026020_gabriel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.posttest4_1915026020_gabriel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_page.dart
@@ -0,0 +1,414 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var heigt = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: const Color(0xffEDF2F4),
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: heigt / 4),
+                width: 90,
+                height: 90,
+                decoration: const BoxDecoration(
+                  image:
+                      DecorationImage(image: AssetImage("assets/Palang.png")),
+                ),
+              ),
+              const Text(
+                "AYO DONOR",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xffD80032),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 70, right: 70, top: 10),
+                child: Text(
+                  "Aplikasi Screening untuk pendonor darah sebelum melakukan pendonoran darah \n by Gabriel D Lawrence",
+                  style: TextStyle(
+                    fontSize: 8,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xff8d99ae),
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              const MyButton(),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  const MyButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) {
+          return MainPage();
+        }));
+      },
+      child: Container(
+        margin: EdgeInsets.only(top: 200),
+        width: 215,
+        height: 60,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          color: Color(0xffEF233C),
+        ),
+        child: const Text(
+          "Start Screening",
+          style: TextStyle(
+            fontSize: 18,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.w600,
+            color: Color(0xffEDF2F4),
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var tinggi = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Image.asset(
+              "assets/logo1.png",
+              fit: BoxFit.contain,
+              height: 28,
+            ),
+            const Text(
+              "  AYO DONOR",
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            )
+          ],
+        ),
+        backgroundColor: Color(0xffba0001),
+      ),
+      backgroundColor: Colors.grey[300],
+      body: Stack(
+        children: [
+          ListView(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: 15),
+                    child: Text(
+                      "Selamat Datang, user",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 15),
+                    child: Text(
+                      "Aplikasi ini akan memudahkan kamu melakukan screening tanpa perlu datang kerumah sakit atau tempat mendonorkan darah lainnya. Aplikasi ini juga akan menunjukkan kelayakanmu mendonorkan darah berdasarkan dari jawaban kamu di form screening kami.",
+                      style: TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 10),
+                    child: Text(
+                      "Jadi mohon mengisi dengan penuh tanggung jawab.",
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: tinggi / 6),
+                    width: 200,
+                    height: 60,
+                    decoration: BoxDecoration(
+                        color: Color(0xffEF233C),
+                        borderRadius: BorderRadius.circular(12),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.shade500,
+                            offset: Offset(6, 6),
+                            blurRadius: 15,
+                            spreadRadius: 1,
+                          ),
+                          BoxShadow(
+                            color: Colors.white,
+                            offset: Offset(-6, -6),
+                            blurRadius: 5,
+                            spreadRadius: 1,
+                          ),
+                        ]),
+                    alignment: Alignment.center,
+                    child: Text("Lakukan Screening",
+                        style: TextStyle(
+                          color: Colors.grey[300],
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                        )),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              width: double.infinity,
+              height: 60,
+              decoration: BoxDecoration(
+                color: Color(0xff7f7f7f),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushReplacement(context,
+                          MaterialPageRoute(builder: (_) {
+                        return LandingPage();
+                      }));
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Icon(Icons.home, color: Colors.white),
+                        Text(
+                          "Home",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Icon(Icons.bloodtype, color: Color(0xffEF233C)),
+                      Text(
+                        "Education",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (_) {
+                        return ProfilePage();
+                      }));
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Icon(Icons.perm_contact_cal, color: Colors.white),
+                        Text(
+                          "Profile",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+enum Survive { laki, perempuan }
+
+class _ProfilePageState extends State<ProfilePage> {
+  final controllerNama = TextEditingController();
+  final controllerAlamat = TextEditingController();
+  final controllerGoldar = TextEditingController();
+  String nama = "", alamat = "", goldar = "", gender = "", outputGender = "";
+  bool submit = false;
+  Survive? kelamin = Survive.laki;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Image.asset(
+              "assets/logo1.png",
+              fit: BoxFit.contain,
+              height: 28,
+            ),
+            Text(
+              " AYO DONOR",
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            )
+          ],
+        ),
+        backgroundColor: Color(0xffba0001),
+      ),
+      body: ListView(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  "Masukkan data diri anda : ",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: TextFormField(
+                  controller: controllerNama,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Nama",
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: TextFormField(
+                  controller: controllerAlamat,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Alamat",
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: TextFormField(
+                  controller: controllerGoldar,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Golongan Darah",
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Text("Jenis Kelamin : "),
+              ),
+              Row(
+                children: [
+                  Radio(
+                      activeColor: Color(0xffba0001),
+                      value: Survive.laki,
+                      groupValue: kelamin,
+                      onChanged: (Survive? value) {
+                        setState(() {
+                          kelamin = value;
+                        });
+                        if (value == Survive.laki) {
+                          gender = "Laki-laki";
+                        }
+                      }),
+                  Text("Laki - laki"),
+                ],
+              ),
+              Row(
+                children: [
+                  Radio(
+                      activeColor: Color(0xffba0001),
+                      value: Survive.perempuan,
+                      groupValue: kelamin,
+                      onChanged: (Survive? value) {
+                        setState(() {
+                          kelamin = value;
+                          if (value == Survive.perempuan) {
+                            gender = "Perempuan";
+                          }
+                        });
+                      }),
+                  Text("Perempuan"),
+                ],
+              ),
+              Center(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xffEF233C),
+                  ),
+                  onPressed: (() {
+                    setState(() {
+                      nama = "Nama anda : " + controllerNama.value.text;
+                      alamat =
+                          "Alamat anda di : " + controllerAlamat.value.text;
+                      goldar = "Golongan darah anda : " +
+                          controllerGoldar.value.text;
+                      outputGender = "Jenis Kelamin anda : " + gender;
+                    });
+                    submit = true;
+                  }),
+                  child: Text("Submit"),
+                ),
+              ),
+              if (submit) ...[
+                Text(
+                  "Data diri anda : ",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                Text("$nama"),
+                Text("$alamat"),
+                Text("$goldar"),
+                Text("$outputGender"),
+              ],
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
